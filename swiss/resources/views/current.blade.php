66045772512ffd07538b05bd4dc51cd8a2594012
xDoni <dinof98@gmail.com>
2018-03-28 11:30:28 +0200

    Tables for the players in the current tournament

--- swiss/resources/views/current.blade.php
+++ swiss/resources/views/current.blade.php
@@ -2,4 +2,76 @@
 
 @section('content')
 
+<div class="container-fluid">
+
+	<div class="row justify-content-center">
+
+		<div class="col-md-8">
+
+			<h1 class="text-center">Current tournament</h1>
+
+			<hr>
+
+			<table class="col-md-2"> 
+
+				<tbody>
+
+					<tr>
+
+						<td class="col-md-2">Dino Felarca</td>
+
+						<td class="col-md-8">VS</td>
+
+						<td class="col-md-2">@mdo</td>
+
+					</tr>
+
+					<tr>
+
+						<td class="col-md-2">Jacob</td>
+
+						<td class="col-md-8">VS</td>
+
+						<td class="col-md-2">@fat</td>
+
+					</tr>
+
+				</tbody>
+
+			</table>
+
+			<table class="col-md-2"> 
+
+				<tbody>
+
+					<tr>
+
+						<td class="col-md-2">Mark</td>
+
+						<td class="col-md-8">VS</td>
+
+						<td class="col-md-2">@mdo</td>
+
+					</tr>
+
+					<tr>
+
+						<td class="col-md-2">Jacob</td>
+
+						<td class="col-md-8">VS</td>
+
+						<td class="col-md-2">@fat</td>
+
+					</tr>
+
+				</tbody>
+
+			</table>
+
+		</div>
+
+	</div>
+
+</div>
+
 @endsection
