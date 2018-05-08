619925ab5a9ec88f95251d73df80eef9bcc5501d
Dino Felarca <dino.felarca@nti.umea>
2018-05-08 14:01:58 +0200

    Revert "Merge branch 'master' of https://github.com/BerraBelin96/swiss"
    This reverts commit 0452787aa5a9353d4f55a799b227adc33b0a6e2b, reversing
    changes made to b079d6f8d0862d1a831c9c12db73c7c1d9945f5c.

--- swiss/resources/views/index.blade.php
+++ swiss/resources/views/index.blade.php
@@ -13,42 +13,42 @@
     <div class="row justify-content-center" id="tournament-btn">
         <div class="col-md-8">
             <form method="POST" action="/createTournament">
-               {{ csrf_field() }}
-               <div class="input-group col-md-10 mx-auto">
-                   <input type="text" class="form-control" id="name" name="name"
-                   placeholder="Create a tournament" autofocus> <span class="input-group-btn">
-                       <button type="submit" class="btn btn-secondary">Create</button>
-                   </span>
-               </div>
-               @include('layouts.errors')
-           </form>
-           <br>
-           <button type="button" class="btn btn-secondary"><a href="#">Tournament history</a></button>
-           <hr>
-       </div>
-   </div>
-   @endguest
-</div>
-<div class="container-fluid">
-    <div class="row justify-content-center col-md-8 mx-auto">
-       @foreach ($tournament as $tournaments)
-       @if ($tournaments->status != 'finished')
-       @guest
-       <a class="col-md-12" href="{{ URL::to('/current/'.$tournaments->id) }}">
+             {{ csrf_field() }}
+             <div class="input-group col-md-10 mx-auto">
+                 <input type="text" class="form-control" id="name" name="name"
+                 placeholder="Create a tournament" autofocus> <span class="input-group-btn">
+                     <button type="submit" class="btn btn-secondary">Create</button>
+                 </span>
+             </div>
+             @include('layouts.errors')
+         </form>
+         <br>
+         <button type="button" class="btn btn-secondary"><a href="#">Tournament history</a></button>
+         <hr>
+     </div>
+ </div>
+ @endguest
+ </div>
+ <div class="container-fluid">
+ <div class="row justify-content-center col-md-8 mx-auto">
+         @foreach ($tournament as $tournaments)
+         @if ($tournaments->status != 'finished')
+         @guest
+         <a class="col-md-12" href="{{ URL::to('/current/'.$tournaments->id) }}">
         @else
         <a class="col-md-12" href="{{ URL::to('admin/current/'.$tournaments->id) }}">
             @if ( $tournaments->status == 'new' )
-            <a class="col-md-12" href="{{ URL::to('admin/create/'.$tournaments->id) }}">
-                @endif
-                @endguest
-                <div id="tournamentName">
-                    <span>{{ $tournaments->name }}</span> 
-                    <span class="status">{{ $tournaments->status }}
-                    </span>
-                </div>
-            </a>
+                <a class="col-md-12" href="{{ URL::to('admin/create/'.$tournaments->id) }}">
             @endif
-            @endforeach
+        @endguest
+         <div id="tournamentName">
+            <span>{{ $tournaments->name }}</span> 
+            <span class="status">{{ $tournaments->status }}
+            </span>
         </div>
-    </div>
-    @endsection
+        </a>
+        @endif
+         @endforeach
+         </div>
+         </div>
+ @endsection
